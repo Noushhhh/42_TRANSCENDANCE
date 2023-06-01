c978c34fde3a658865a5444950f287369d70f498
polo <you@example.com>
2023-06-01 14:42:07 +0200

    container react redirected to localhost:8080 // node_modules removed from repo and added to gitignore

--- srcs/files/frontend/app/src/App.js
+++ srcs/files/frontend/app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and le sexe to reload.
+          Edit <code>src/App.js</code> and le MAX et le sexe to reload.
         </p>
         <a
           className="App-link"
