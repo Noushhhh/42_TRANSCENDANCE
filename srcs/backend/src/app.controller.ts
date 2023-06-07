0445719c11c7a6451a950028dc3c32a46fa2a68d
Noushhhh <ana.andric@live.fr>
2023-06-07 12:15:08 +0200

    feat (architecture of project and folders) : updated to have cleaner folder architectures more logical

--- srcs/backend/src/app.controller.ts
+++ srcs/backend/src/app.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service'
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) { }
+import { AppService } from './app.service';
 
-  @Get()
+@Controller() // Declare this class as a controller
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get() // Handles GET requests to the root URL
   getHello(): string {
     // Call the `getHello` method of the `AppService`
     return this.appService.getHello();
-  }
-  @Get('status') // Handles GET requests to the root URL
-  getStatus(): { status: string } {
-    return { status: 'ok' };
   }
 }
