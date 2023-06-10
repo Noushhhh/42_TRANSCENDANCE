528d508b3232a8aa50ca4ca3eafeb92e0bb21e35
polo <you@example.com>
2023-06-10 12:55:23 +0200

    main to 18-chat branch

--- srcs/frontend/app/src/index.tsx
+++ srcs/frontend/app/src/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Navbar from './navbar/navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Navbar />
     <App />
   </React.StrictMode>
 );
