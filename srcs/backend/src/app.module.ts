2b4cb80a33556e284ecf78b239fe95fec0c8e799
Noushhhh <ana.andric@live.fr>
2023-06-13 15:37:16 +0200

    feat (Post from Auth) : functionning with insomnia

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -1,24 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+// import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+// import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.POSTGRES_HOST || 'localhost',
-    //   port: 5432,
-    //   username: process.env.POSTGRES_USER,
-    //   password: process.env.POSTGRES_PASSWORD,
-    //   database: process.env.POSTGRES_DB,
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
-    AuthModule, UserModule, BookmarkModule
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    // imports: [
+    //   ConfigModule.forRoot({
+    //     isGlobal: true,
+    //    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
   ],
   controllers: [AppController],
   providers: [AppService],
