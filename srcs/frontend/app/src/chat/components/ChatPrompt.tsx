54dd618e155d1c5e16915466851bbb2bc5f50f3b
polo <you@example.com>
2023-06-14 10:41:05 +0200

    feat (/frontend/chat) : ChatView components updated when user fill the ChatPrompt component an press send button

--- srcs/frontend/app/src/chat/components/ChatPrompt.tsx
+++ srcs/frontend/app/src/chat/components/ChatPrompt.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import "../styles/ChatPrompt.css"
 import SendIcon from '@mui/icons-material/Send';
 
-function ChatPrompt() {
+interface ChatPromptProps{
+  addMessage:(newMessage: string) => void;
+}
+
+function ChatPrompt({addMessage}: ChatPromptProps): JSX.Element {
+
+	const [message, setMessage] = useState("");
+
+	const handleMessageChange = (event: ChangeEvent<HTMLInputElement>): void =>{
+		setMessage(event.target.value);
+	}
+
+	function isWhitespace(str: string): boolean {
+		return /^\s*$/.test(str);
+	}
+
+	const handleEvent = () => {
+		if ( ! isWhitespace(message))
+			addMessage(message);
+		setMessage("");
+	}
+
     return (
         <div className="ChatPrompt">
-            <input className="InputChatPrompt" type="text"/>
-            <span className="SendIconPromptChat"><SendIcon/></span>
+            <input	value={message}
+					className="InputChatPrompt"
+					onChange={handleMessageChange} 
+					type="text"/>
+            <button className="SendIconPromptChat" onClick={handleEvent}>
+            	<SendIcon/>
+            </button>
         </div>
     )   
 }
--- srcs/frontend/app/src/chat/components/ContentMessage.tsx
+++ srcs/frontend/app/src/chat/components/ContentMessage.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import "../styles/ContentMessage.css";
 import HeaderChatBox from "./HeaderChatBox";
 import ChatView from "./ChatView";
 import ChatPrompt from "./ChatPrompt";
+import { useState } from "react";
+import "../styles/ContentMessage.css";
+
 
 function ContentMessage() {
+
+    const [messages, setMessages] = useState<string[]>([]);
+
+    const addMessage = (newMessage: string): void =>{
+        setMessages([...messages, newMessage]);
+    }
+
     return (
         <div className="ContentMessage">
             <HeaderChatBox />
-            <ChatView />
-            <ChatPrompt/>
+            <ChatView messages={messages}/>
+            <ChatPrompt addMessage={addMessage}/>
         </div>
     )   
 }
 
 export default ContentMessage;
+
+// declarer un useState dans le composant parent
+// declarer un setter dans le composant parent
+// passer le setter au composant prompt
+// utiliser le setter dans le composant prompt afin de passer la donnée au parents
+// renvoyer la donnée du parent au composant qui render les messages
