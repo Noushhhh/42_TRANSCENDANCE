1dcb0acd796a6a963f5bc7caec0bc80a6a4207d6
Théo <74186192+Theoted@users.noreply.github.com>
2023-06-27 11:46:40 +0200

    feature (server authority) I've added all the game logic in the backend and I've created a gameloop

--- srcs/frontend/app/src/game/components/Paddles.tsx
+++ srcs/frontend/app/src/game/components/Paddles.tsx
@@ -4,6 +4,26 @@ import { Rect } from "react-konva";
 import * as data from "../assets/data";
 import { Vector2d } from "konva/lib/types";
 
+interface gameState {
+  p1pos: {
+    x: number;
+    y: number;
+  };
+  p2pos: {
+    x: number;
+    y: number;
+  },
+  ballPos: {
+    x: number;
+    y: number;
+  };
+  isPaused: boolean;
+  score: {
+    p1Score: number;
+    p2Score: number;
+  };
+}
+
 const Paddles: FC<data.PaddleProps> = ({
   socket,
   isPaused = false,
@@ -14,42 +34,138 @@ const Paddles: FC<data.PaddleProps> = ({
   const rect2Ref = useRef<Konva.Rect>(null);
   const keyState = useRef<{ [key: string]: boolean }>({});
 
-  console.log("lu");
-
   useEffect(() => {
-    socket.on("updateP1Pos", (res: Vector2d) => {
-      updateP1Pos(res);
+    socket.on("updateGameState", (gameState: gameState) => {
+      updateP1pos(gameState.p1pos);
+      updateP2pos(gameState.p2pos);
     });
-    socket.on("updateP2Pos", (res: Vector2d) => {
-      updateP2Pos(res);
-    });
+
     return () => {
-      socket.off("updateP1Pos");
-      socket.off("updateP2Pos");
+      socket.off("updateGameState");
     };
   }, []);
 
-  const sendP1Pos = () => {
-    socket.emit("getP1Pos", p1Pos.current);
-  };
+  // useEffect(() => {
+  //   socket.on("updateP1Pos", (res: Vector2d) => {
+  //     updateP1Pos(res);
+  //   });
+  //   socket.on("updateP2Pos", (res: Vector2d) => {
+  //     updateP2Pos(res);
+  //   });
+  //   return () => {
+  //     socket.off("updateP1Pos");
+  //     socket.off("updateP2Pos");
+  //   };
+  // }, []);
 
-  const updateP1Pos = (pos: Vector2d) => {
+  // const sendP1Pos = () => {
+  //   socket.emit("getP1Pos", p1Pos.current);
+  // };
+
+  // const updateP1Pos = (pos: Vector2d) => {
+  //   const rect1 = rect1Ref.current;
+  //   if (!rect1) return;
+
+  //   rect1.position({ x: pos.x, y: pos.y });
+  //   p1Pos.current = rect1.position();
+  // };
+
+  // const sendP2Pos = () => {
+  //   socket.emit("getP2Pos", p2Pos.current);
+  // };
+
+  // const updateP2Pos = (pos: Vector2d) => {
+  //   const rect2 = rect2Ref.current;
+  //   if (!rect2) return;
+
+  //   rect2.position({ x: pos.x, y: pos.y });
+  //   p2Pos.current = rect2.position();
+  // };
+
+  // useEffect(() => {
+  //   const handleKeyDown = (event: globalThis.KeyboardEvent) => {
+  //     keyState.current[event.key] = true;
+  //   };
+
+  //   const handleKeyUp = (event: globalThis.KeyboardEvent) => {
+  //     keyState.current[event.key] = false;
+  //   };
+
+  //   let animRequest: number;
+
+  //   const updateRect = () => {
+  //     if (isPaused) return;
+
+  //     const rect1 = rect1Ref.current;
+  //     if (!rect1) return;
+
+  //     const rect2 = rect2Ref.current;
+  //     if (!rect2) return;
+
+  //     const position1 = rect1.position();
+  //     const position2 = rect2.position();
+  //     const moveAmount = 6;
+
+  //     if (keyState.current["w"]) {
+  //       if (position1.y > 0) {
+  //         rect1.position({ x: position1.x, y: position1.y - moveAmount });
+  //         p1Pos.current = rect1.position();
+  //         sendP1Pos();
+  //       }
+  //     }
+  //     if (keyState.current["s"]) {
+  //       if (position1.y < data.KONVA_HEIGHT - data.PADDLE_HEIGHT) {
+  //         rect1.position({ x: position1.x, y: position1.y + moveAmount });
+  //         p1Pos.current = rect1.position();
+  //         sendP1Pos();
+  //       }
+  //     }
+  //     if (keyState.current["ArrowUp"]) {
+  //       if (position2.y > 0) {
+  //         rect2.position({ x: position2.x, y: position2.y - moveAmount });
+  //         p2Pos.current = rect2.position();
+  //         sendP2Pos();
+  //       }
+  //     }
+  //     if (keyState.current["ArrowDown"]) {
+  //       if (position2.y < data.KONVA_HEIGHT - data.PADDLE_HEIGHT) {
+  //         rect2.position({ x: position2.x, y: position2.y + moveAmount });
+  //         p2Pos.current = rect2.position();
+  //         sendP2Pos();
+  //       }
+  //     }
+  //     animRequest = requestAnimationFrame(updateRect);
+  //   };
+
+  //   window.addEventListener("keydown", handleKeyDown);
+  //   window.addEventListener("keyup", handleKeyUp);
+  //   const animRequest2 = requestAnimationFrame(updateRect);
+
+  //   return () => {
+  //     cancelAnimationFrame(animRequest);
+  //     cancelAnimationFrame(animRequest2);
+  //     window.removeEventListener("keydown", handleKeyDown);
+  //     window.removeEventListener("keyup", handleKeyUp);
+  //   };
+  // }, [isPaused]);
+
+  const updateP1pos = (pos: Vector2d) => {
     const rect1 = rect1Ref.current;
     if (!rect1) return;
 
-    rect1.position({ x: pos.x, y: pos.y });
+    requestAnimationFrame(() => {
+      rect1.position({ x: pos.x, y: pos.y });
+    })
     p1Pos.current = rect1.position();
   };
 
-  const sendP2Pos = () => {
-    socket.emit("getP2Pos", p2Pos.current);
-  };
-
-  const updateP2Pos = (pos: Vector2d) => {
+  const updateP2pos = (pos: Vector2d) => {
     const rect2 = rect2Ref.current;
     if (!rect2) return;
 
-    rect2.position({ x: pos.x, y: pos.y });
+    requestAnimationFrame(() => {
+      rect2.position({ x: pos.x, y: pos.y });
+    })
     p2Pos.current = rect2.position();
   };
 
@@ -67,43 +183,17 @@ const Paddles: FC<data.PaddleProps> = ({
     const updateRect = () => {
       if (isPaused) return;
 
-      const rect1 = rect1Ref.current;
-      if (!rect1) return;
-
-      const rect2 = rect2Ref.current;
-      if (!rect2) return;
-
-      const position1 = rect1.position();
-      const position2 = rect2.position();
-      const moveAmount = 6;
-
       if (keyState.current["w"]) {
-        if (position1.y > 0) {
-          rect1.position({ x: position1.x, y: position1.y - moveAmount });
-          p1Pos.current = rect1.position();
-          sendP1Pos();
-        }
+        socket.emit("getP1Pos", "up");
       }
       if (keyState.current["s"]) {
-        if (position1.y < data.KONVA_HEIGHT - data.PADDLE_HEIGHT) {
-          rect1.position({ x: position1.x, y: position1.y + moveAmount });
-          p1Pos.current = rect1.position();
-          sendP1Pos();
-        }
+        socket.emit("getP1Pos", "down");
       }
       if (keyState.current["ArrowUp"]) {
-        if (position2.y > 0) {
-          rect2.position({ x: position2.x, y: position2.y - moveAmount });
-          p2Pos.current = rect2.position();
-          sendP2Pos();
-        }
+        socket.emit("getP2Pos", "up");
       }
       if (keyState.current["ArrowDown"]) {
-        if (position2.y < data.KONVA_HEIGHT - data.PADDLE_HEIGHT) {
-          rect2.position({ x: position2.x, y: position2.y + moveAmount });
-          p2Pos.current = rect2.position();
-          sendP2Pos();
-        }
+        socket.emit("getP2Pos", "down");
       }
       animRequest = requestAnimationFrame(updateRect);
     };
