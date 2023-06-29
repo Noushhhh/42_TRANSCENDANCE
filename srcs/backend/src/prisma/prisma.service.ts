2764cc600627806d3948e2d9d54c1857110a2e2f
polo <you@example.com>
2023-06-29 14:10:21 +0200

    style prisma.service.ts

--- srcs/backend/src/prisma/prisma.service.ts
+++ srcs/backend/src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL                },
+                    url: process.env.DATABASE_URL},
             },
         });
     }
