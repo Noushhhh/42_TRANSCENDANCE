8abdf1d7128bb5b1b68a7e7524a88199f76a6e43
Noushhhh <ana.andric@live.fr>
2023-07-10 17:59:23 +0200

    feat (prisma on localhost:5555)

--- srcs/backend/src/auth/auth.service.ts
+++ srcs/backend/src/auth/auth.service.ts
@@ -1,27 +1,46 @@
 import { Injectable } from '@nestjs/common';
 // import { User, Bookmark } from  '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    async signup(dto: AuthDto) {
+    async signup(dto: AuthDto) 
+    {
         // generate password hash
         const hashPassword = await argon.hash(dto.password);
         // save user in db
-        const user = await this.prisma.user.create({
-            data: {
-                username: dto.username,
-                hashPassword,
-            },
-        });
-        // return saved user
-        return user;
-        return { msg: 'I have signed up' };
+        try
+        {
+            const user = await this.prisma.user.create({
+                data: {
+                    username: dto.username,
+                    hashPassword,
+                },
+            select:
+            {
+                username : true, // return only username and not hash pwd
+            }
+            });
+            // return saved user
+            return user;
+        }
+        catch (error: unknown) {
+            // handle parsing errors
+            if (
+                error instanceof PrismaClientKnownRequestError &&
+                error.code === 'P2002'
+            ) {
+                throw new Error('Username already in use');
+            }
+    
+            // handle other errors
+            throw error;
+        }
     }
 
     signin() {
