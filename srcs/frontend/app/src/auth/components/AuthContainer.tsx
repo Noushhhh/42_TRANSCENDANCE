a6e5314957fb0524804da665d4b1f095549362b5
Noushhhh <ana.andric@live.fr>
2023-07-24 15:38:54 +0200

    merge with main

---
+++ srcs/frontend/app/src/auth/components/AuthContainer.tsx
@@ -0,0 +1,11 @@
+// import React, { useEffect, useState } from "react";
+// import "../styles/AuthContainer.css";
+
+// function AuthContainer() {
+//   return (
+//     <div className="AuthContainer">
+//     </div>
+//   );
+// }
+
+// export default AuthContainer;
