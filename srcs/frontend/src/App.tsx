3771f2bda9d91dd6b65b740e9f0370c769a69a8d
Alexvc23 <alexandervalencia1994@gmail.com>
2023-08-28 15:13:07 +0200

    feat (routing) : added catch-all route to render ErrorComponent for unmatched paths

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -1,38 +1,33 @@
-import GameContainer from "./game/components/GameContainer";
-import Navbar from "./navbar/navbar";
-import ChatBoxContainer from "./chat/components/ChatBoxContainer";
-// import AuthContainer from './auth/components/AuthContainer';
-
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import {
-  Welcome, SignIn, SignUp, AuthChoise, HomePage, Settings,
-  ProtectedRoute, Friends, Stats, Chat, useActivityLogout
-} from './home/components/index'
-import './App.css'
-
-{/* <Navbar /> */ }
-{/* <ChatBoxContainer /> */ }
-{/* <GameContainer /> */ }
-{/* < AuthContainer/> */ }
+    Welcome, SignIn, SignUp, AuthChoise, HomePage, Settings,
+    ProtectedRoute, Friends, Stats, ErrorComponent, useActivityLogout
+} from './home/components/index';
+import GameContainer from "./game/components/GameContainer";
+import ChatBoxContainer from "./chat/components/ChatBoxContainer";
+import './App.css';
 
 const App: React.FC = () => {
+    // Hooks: Use the `useActivityLogout` hook to check for user inactivity and perform a logout if needed.
     useActivityLogout();
     return (
-            <Routes>
-                <Route path="/" element={<Welcome />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/authchoise" element={<AuthChoise />} />
-                <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>}>
-                    <Route path="chat" element={<ChatBoxContainer />} />
-                    <Route path="friends" element={<Friends />} />
-                    <Route path="stats" element={<Stats />} />
-                    <Route path="settings" element={<Settings />} />
-                    <Route path="game" element={<GameContainer />} />
-                </Route>
-            </Routes>
+        <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/authchoise" element={<AuthChoise />} />
+            <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>}>
+                <Route path="chat" element={<ChatBoxContainer />} />
+                <Route path="friends" element={<Friends />} />
+                <Route path="stats" element={<Stats />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="game" element={<GameContainer />} />
+            </Route>
+            {/*       https://stackoverflow.com/questions/74645355/react-router-v6-catch-all-path-does-not-work-when-using-nested-routes */}
+            <Route path="*" element={<ErrorComponent />} /> {/* Catch-all route */}
+        </Routes>
     );
-}
-export default App;
+};
 
+export default App;
