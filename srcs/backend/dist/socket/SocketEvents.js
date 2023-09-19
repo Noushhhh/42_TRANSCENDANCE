c6e78203d8d9bb1a4b9848b15ee0c5a70f049e2a
Noushhhh <ana.andric@live.fr>
2023-09-19 15:37:32 +0200

    Update SocketEvents.js

--- srcs/backend/dist/socket/SocketEvents.js
+++ srcs/backend/dist/socket/SocketEvents.js
@@ -11,10 +11,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SocketEvents = void 0;
 const websockets_1 = require("@nestjs/websockets");
 const socket_io_1 = require("socket.io");
+const client_1 = require("@prisma/client");
+let SocketEvents = exports.SocketEvents = class SocketEvents {
+    constructor() {
         this.listUserConnected = new Map();
     }
     onModuleInit() {
@@ -31,8 +35,11 @@ const socket_io_1 = require("socket.io");
         console.log(Array.from(map.values()));
     }
     handleSetNewUserConnected(userId, client) {
+        console.log("user conneted");
+        console.log(userId);
         this.listUserConnected.set(userId, client.id);
         this.server.emit("changeConnexionState");
+        this.readMap(this.listUserConnected);
     }
     handleIsUserConnected(userId, client) {
         const socketId = this.listUserConnected.get(userId); // get the socketId from userId
@@ -86,6 +93,7 @@ __decorate([
     __param(0, (0, websockets_1.MessageBody)()),
     __param(1, (0, websockets_1.ConnectedSocket)()),
     __metadata("design:type", Function),
+    __metadata("design:paramtypes", [typeof (_a = typeof client_1.Message !== "undefined" && client_1.Message) === "function" ? _a : Object, socket_io_1.Socket]),
     __metadata("design:returntype", void 0)
 ], SocketEvents.prototype, "handleMessage", null);
 exports.SocketEvents = SocketEvents = __decorate([
