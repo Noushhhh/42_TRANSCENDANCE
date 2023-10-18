7c62b91bab4c7edeb588ef5e4fa86284f1bfdd08
polo <you@example.com>
2023-10-18 17:30:39 +0200

    65 safe to main

--- srcs/backend/dist/socket/socket.service.js
+++ srcs/backend/dist/socket/socket.service.js
@@ -1,16 +1,28 @@
 "use strict";
-// import { Injectable } from '@nestjs/common';
-// import { Socket } from 'socket.io';
-// @Injectable()
-// export class SocketService {
-//   private socketMap: Map<string, Socket> = new Map();
-//   setSocket(clientId: string, socket: Socket) {
-//     this.socketMap.set(clientId, socket);
-//   }
-//   getSocket(clientId: string) {
-//     return this.socketMap.get(clientId);
-//   }
-//   removeSocket(clientId: string) {
-//     this.socketMap.delete(clientId);
-//   }
-// }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SocketService = void 0;
+const common_1 = require("@nestjs/common");
+let SocketService = class SocketService {
+    constructor() {
+        this.socketMap = new Map();
+    }
+    setSocket(clientId, socket) {
+        this.socketMap.set(clientId, socket);
+    }
+    getSocket(clientId) {
+        return this.socketMap.get(clientId);
+    }
+    removeSocket(clientId) {
+        this.socketMap.delete(clientId);
+    }
+};
+exports.SocketService = SocketService;
+exports.SocketService = SocketService = __decorate([
+    (0, common_1.Injectable)()
+], SocketService);
--- srcs/backend/src/socket/socket.module.ts
+++ srcs/backend/src/socket/socket.module.ts
@@ -1,7 +1,8 @@
-// import { Module } from "@nestjs/common";
-// import { ChatService } from "../chat/chat.service";
+import { Module } from "@nestjs/common";
+import { ChatService } from "../chat/chat.service";
+import { SocketService } from "./socket.service";
 
-// @Module({
-//     providers:[ChatService],
-// })
-// export class SocketModule {};
+@Module({
+    providers:[ChatService, SocketService],
+})
+export class SocketModule {};
