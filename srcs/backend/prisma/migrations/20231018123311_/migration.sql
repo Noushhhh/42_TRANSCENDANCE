f6d850fd81139eef312f4e844b0e03e1ae7c4301
polo <you@example.com>
2023-10-20 16:05:27 +0200

    after rm --cached to remove dist folder from actual index

---
+++ srcs/backend/prisma/migrations/20231018123311_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "loggedIn" BOOLEAN NOT NULL DEFAULT false;
---
+++ srcs/backend/src/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+
+export interface UserProfileData {
+  publicName: string | null;
+  avatar: string | null;
+}
