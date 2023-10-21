4dfe30f1f2b2e661db8cdd343ffe4dc266e36d37
Alexvc23 <alexandervalencia1994@gmail.com>
2023-10-21 06:17:20 +0200

    fix (beforeunload even added in react): this will disconnect the client when leaving page, so we will mark in the data base the logging state  as false

--- srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
+++ srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
@@ -1,33 +1,54 @@
+/**
+ * @file useActivityLogout.tsx
+ * @author [Jairo Alexander VALENCIA CANDAMIL]
+ * @date 2023-10-21
+ * 
+ * @brief This is a custom React hook that handles user inactivity.
+ * It logs out the user after a certain period of inactivity and navigates to the sign-in page.
+ * 
+ * @note The inactivity period is currently set to 120000 milliseconds (2 minutes).
+ */
+
 // Importing necessary hooks and components from "react-router-dom".
 import { useNavigate } from "react-router-dom";
 import { useSignOut } from "./useSignOut";
 import { useEffect } from "react";
 
+/**
+ * @function useActivityLogout
+ * @description This hook sets up event listeners for user activity and logs out the user after a period of inactivity.
+ * @returns {null} This hook does not return anything.
+ */
 const useActivityLogout = () => {
     const navigate = useNavigate();
     const handleSignOut = useSignOut();
     let inactivityTimer: any;
 
+    // Function to handle user sign out and navigation to the sign-in page.
     const logoutAndNavigate = () => {
         handleSignOut();
         navigate('signin');
     };
 
+    // Function to reset the inactivity timer.
     const resetTimer = () => {
         clearTimeout(inactivityTimer);
         inactivityTimer = setTimeout(logoutAndNavigate, 120000);
     };
 
+    // Function to handle window close event.
     const handleWindowClose = () => {
         handleSignOut();
     };
 
+    // Setting up event listeners for user activity and window close.
     useEffect(() => {
         window.addEventListener('load', resetTimer);
         window.addEventListener('mousemove', resetTimer);
         window.addEventListener('keypress', resetTimer);
         window.addEventListener('beforeunload', handleWindowClose);
 
+        // Cleaning up event listeners on unmount.
         return () => {
             window.removeEventListener('load', resetTimer);
             window.removeEventListener('mousemove', resetTimer);
