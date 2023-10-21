0184a1440497e279f420d2d03bbaa987b6a2676c
Alexvc23 <alexandervalencia1994@gmail.com>
2023-10-21 06:10:04 +0200

    fix (beforeunload even added in react): this will disconnect the client when leaving page, so we will mark in the data base the logging state  as false

--- srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
+++ srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
@@ -1,48 +1,42 @@
 // Importing necessary hooks and components from "react-router-dom".
 import { useNavigate } from "react-router-dom";
 import { useSignOut } from "./useSignOut";
+import { useEffect } from "react";
 
-// Hook to handle automatic logout after a period of inactivity.
 const useActivityLogout = () => {
-
-    // Utilize the `useNavigate` hook from react-router to programmatically change routes.
     const navigate = useNavigate();
     const handleSignOut = useSignOut();
+    let inactivityTimer: any;
 
-    // Variable to store the inactivity timer.
-    let inicativityTimer: any;
-
-    // Function to remove the user token and navigate to the signin page.
     const logoutAndNavigate = () => {
         handleSignOut();
-        navigate('signin'); // Navigate to signin page.
+        navigate('signin');
     };
 
-    // Reset the inactivity timer whenever there's user activity.
     const resetTimer = () => {
-        clearTimeout(inicativityTimer); // Clear any existing timers.
-
-        // Set a new timeout for automatic logout after 20 minutes of inactivity.
-        inicativityTimer = setTimeout(logoutAndNavigate, 120000);
+        clearTimeout(inactivityTimer);
+        inactivityTimer = setTimeout(logoutAndNavigate, 120000);
     };
 
-    // Adding event listeners for various user activities to reset the inactivity timer.
-
-    // Reset timer on page load.
-    window.addEventListener('load', resetTimer);
-
-    // Reset timer on mouse movement.
-    window.addEventListener('mousemove', resetTimer);
-
-    // Reset timer on key press.
-    window.addEventListener('keypress', resetTimer);
-
-    // Cleanup function: Remove the event listeners when the hook is no longer in use.
-    return () => {
-        window.removeEventListener('load', resetTimer);
-        window.removeEventListener('mousemove', resetTimer);
-        window.removeEventListener('keypress', resetTimer);
+    const handleWindowClose = () => {
+        handleSignOut();
     };
+
+    useEffect(() => {
+        window.addEventListener('load', resetTimer);
+        window.addEventListener('mousemove', resetTimer);
+        window.addEventListener('keypress', resetTimer);
+        window.addEventListener('beforeunload', handleWindowClose);
+
+        return () => {
+            window.removeEventListener('load', resetTimer);
+            window.removeEventListener('mousemove', resetTimer);
+            window.removeEventListener('keypress', resetTimer);
+            window.removeEventListener('beforeunload', handleWindowClose);
+        };
+    }, []);
+
+    return null;
 };
 
 // Export the hook for use in other components.
