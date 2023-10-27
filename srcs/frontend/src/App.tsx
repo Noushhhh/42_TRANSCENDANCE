2faa5ec3174496b2101cc0077ba91d3ff961a1e4
Alexvc23 <alexandervalencia1994@gmail.com>
2023-10-27 14:17:55 +0200

    feature (UserProfile, index.tsx): merge UserProfile, and index profile with error component

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -1,63 +1,40 @@
-import GameContainer from "./game/components/GameContainer";
-import Navbar from "./navbar/navbar";
-import ChatBoxContainer from "./chat/components/ChatBoxContainer";
-// import AuthContainer from './auth/components/AuthContainer';
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import {
-  Welcome,
-  SignIn,
-  SignUp,
-  AuthChoice,
-  HomePage,
-  Settings,
-  ProtectedRoute,
-  Friends,
-  Stats,
-  Chat,
-  useActivityLogout,
-} from "./home/components/index";
-import "./App.css";
+    Welcome, SignIn, SignUp, AuthChoice, HomePage, Settings,
+    ProtectedRoute,  Stats, ErrorComponent, useActivityLogout,
+    UserProfileSetup
+} from './home/components/index';
+// import GameContainer from "./game/components/GameContainer";
+// import ChatBoxContainer from "./chat/components/ChatBoxContainer";
+import './App.css';
 
-{
-  /* <Navbar /> */
-}
-{
-  /* <ChatBoxContainer /> */
-}
-{
-  /* <GameContainer /> */
-}
-{
-  /* < AuthContainer/> */
-}
+// Mock element while Paul finishes the chat, Theo finishes the game, and someone finished the friends
+// component
+const MockComponent: React.FC = () => {
+    return <div> Placeholder for unfinish component </div>
+} 
 
 const App: React.FC = () => {
-
-  useActivityLogout();
-  return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      {/* correct auth instead of authchoice */}
-      <Route path="/authchoice" element={<AuthChoice />} />
-      <Route
-        path="/home"
-        element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        }
-      >
-        {/* add here incremented route if user first connexion can access only user settings page */}
-        <Route path="chat" element={<ChatBoxContainer />} />
-        <Route path="friends" element={<Friends />} />
-        <Route path="stats" element={<Stats />} />
-        <Route path="settings" element={<Settings />} />
-        {/* <Route path="game" element={<GameContainer/>} /> */}
-      </Route>
-    </Routes>
-  );
+    // Hooks: Use the `useActivityLogout` hook to check for user inactivity and perform a logout if needed.
+    useActivityLogout();
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/authchoice" element={<AuthChoice  />} />
+            <Route path="/userprofilesetup" element={<UserProfileSetup />} />
+            <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>}>
+                <Route path="chat" element={<MockComponent/>} /> 
+                <Route path="friends" element={<MockComponent/>} />
+                <Route path="stats" element={<Stats />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="game" element={<MockComponent/>} />
+            </Route>
+            {/*       https://stackoverflow.com/questions/74645355/react-router-v6-catch-all-path-does-not-work-when-using-nested-routes */}
+            <Route path="*" element={<ErrorComponent />} /> {/* Catch-all route */}
+        </Routes>
+    );
 };
 export default App;
---
+++ srcs/frontend/src/home/components/UserProfileSetup.tsx
@@ -0,0 +1,190 @@
+// Import necessary modules and dependencies
+import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styles/generalStyles.css";
+import { useSignOut } from "../tools/hooks/useSignOut";
+
+// Default profile image URL
+const defaultImage = "/assets/defaultProfileImage.jpg";
+
+/**
+* ****************************************************************************
+ * Fetch the default profile image as a File object.
+ * @returns {Promise<File>} A promise that resolves to a File object.
+* ****************************************************************************
+*/
+const fetchDefaultImageAsFile = async () => {
+  const response = await fetch(defaultImage);
+  const data = await response.blob();
+  return new File([data], "defaultImage.jpg", { type: data.type });
+};
+
+/**
+* ****************************************************************************
+ * Update profile on the backend.
+ * @param {File | null} profileImage - The profile image file.
+ * @param {string} profileName - The profile name.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating the success of the update.
+ * @throws {Error} If the update fails.
+* ****************************************************************************
+*/
+const updateProfileBackend = async (profileImage: File | null, profileName: string) => {
+  const formData = new FormData();
+  formData.append("profileImage", profileImage || new Blob());
+  formData.append("profileName", profileName);
+
+  const response = await fetch("http://localhost:8081/api/user/update", {
+    method: "POST",
+    body: formData,
+    credentials: "include",
+  });
+
+  const data = await response.json();
+
+  if (data.valid) {
+    return true;
+  } else {
+    throw new Error(
+      ` ${data?.statusCode}: ${data?.message}` ||
+        "An error occurred when reaching the server to update your profile, please contact the website administrator"
+    );
+  }
+};
+
+/**
+* ****************************************************************************
+ * UserProfileSetup component.
+ * @component
+* ****************************************************************************
+*/
+const UserProfileSetup: React.FC = () => {
+  const location = useLocation();
+  const email = location.state.email;
+
+  // Declare state variables for profile name and profile image
+  const [profileName, setProfileName] = useState("");
+  const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  // Initialize useNavigate and useSignOut hooks
+  const navigate = useNavigate();
+  const handleSignOut = useSignOut();
+
+  // Create a ref for the file input element
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  // Set the default profile image on component mount
+  useEffect(() => {
+    setDefaultImage();
+  }, []);
+
+  // Revoke the object URL for the profile image on component unmount
+  useEffect(() => {
+    return () => {
+      if (profileImage) {
+        URL.revokeObjectURL(URL.createObjectURL(profileImage));
+      }
+    };
+  }, [profileImage]);
+
+/**
+   * Set the default profile image.
+*/
+  const setDefaultImage = async () => {
+    const defaultProfileImage = await fetchDefaultImageAsFile();
+    setProfileImage(defaultProfileImage);
+  };
+
+  /**
+   * Handle profile name input change.
+   * @param {ChangeEvent<HTMLInputElement>} event - The change event.
+   */
+  const handleUsernameChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setProfileName(event.target.value);
+  }, []);
+
+  /**
+   * Handle profile image input change.
+   * @param {ChangeEvent<HTMLInputElement>} event - The change event.
+   */
+  const handleImageChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+
+    if (file) {
+      // Define an array of allowed MIME types for image files
+      const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+
+      // Check if the uploaded file's MIME type is included in the allowedMimeTypes array
+      if (!allowedMimeTypes.includes(file.type)) {
+        // If the file's MIME type is not allowed, show an alert to the user
+        setErrorMessage("Please upload a valid image file.");
+
+        // Clear the file input value if the fileInputRef.current exists
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
+
+        // Stop the function execution
+        return;
+      }
+
+      if (file.size > 15 * 1024 * 1024) {
+        setErrorMessage("Please upload an image smaller than 15MB.");
+
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
+
+        return;
+      }
+
+      setProfileImage(file);
+    }
+  }, []);
+
+  /**
+   * Handle form submission.
+   */
+  const handleSubmit = async () => {
+    if (profileName.trim() === "") {
+      setErrorMessage("Please enter a valid user name");
+      return;
+    }
+
+    try {
+      await updateProfileBackend(profileImage, profileName);
+      navigate("/home");
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message.includes("409")) {
+          setErrorMessage("Username already exists");
+        } else if (error.message.includes("401")) {
+          setErrorMessage("Your credentials have expired, please log in");
+          setTimeout(() => {
+            handleSignOut();
+            navigate("/signin");
+          }, 1000 * 10);
+        } else if (error.message.includes("400")) {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("An unsuspected error occurred. Please try again later");
+        }
+      }
+    }
+  };
+
+  // Render the UserProfileSetup component
+  return (
+    <div className="container">
+      <h1>Pong game</h1>
+      <h2>Welcome {email}</h2>
+      {errorMessage && <div style={{ color: 'white' }}>{errorMessage}</div>}
+      <input type="text" placeholder="Choose a username" value={profileName} onChange={handleUsernameChange} />
+      <input ref={fileInputRef} type="file" onChange={handleImageChange} />
+      <img className="img" src={profileImage ? URL.createObjectURL(profileImage) : defaultImage} alt="Profile preview" />
+      <button onClick={handleSubmit}>Continue</button>
+    </div>
+  );
+};
+
+export default UserProfileSetup;
