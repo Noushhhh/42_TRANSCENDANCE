d9d5aad9a256597591cfcbbbdb6fea334f80ab6b
Alexvc23 <alexandervalencia1994@gmail.com>
2023-10-31 12:31:39 +0100

    feat(Settings): Implement loading state and fix file input error - Added a loading state to the Settings component to show a spinner while fetching user data - Fixed a TypeScript error by passing the first file from the FileList to the checkAndShowInputAvatar function - Improved user experience by handling loading state and error messages more effectively

--- srcs/frontend/src/home/components/Settings.tsx
+++ srcs/frontend/src/home/components/Settings.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { getPublicName, getUserAvatar, updatePublicName, updateAvatar, hasMessage } from '../tools/Api'
-import SignOutLink from '../tools/SignoutLink'
+import { getPublicName, getUserAvatar, updatePublicName, updateAvatar, hasMessage } from '../tools/Api';
+import SignOutLink from '../tools/SignoutLink';
 import { CSSTransition } from 'react-transition-group';
 import '../styles/settings/Settings.css';
 import LoadingSpinner from '../tools/LoadingSpinner'; // New import for a loading spinner component
-
 
 const Settings: React.FC = React.memo(() => {
   const [username, setPublicName] = useState<string | null>(null);
@@ -16,14 +15,6 @@ const Settings: React.FC = React.memo(() => {
   const [promptError, setError] = useState<string | null>(null);
   const [showLoading, setShowLoading] = useState(true);
 
-/*
-
-                       ,---.,---.,---.          |
-        .   .,---.,---.|--- |__. |__. ,---.,---.|---
-        |   |`---.|---'|    |    |    |---'|    |
-        `---'`---'`---'`---'`    `    `---'`---'`---'
-
-*/
   async function fetchPublicName(): Promise<void> {
     const userNameResultFromBack = await getPublicName();
     if (userNameResultFromBack) {
@@ -31,9 +22,8 @@ const Settings: React.FC = React.memo(() => {
     } else {
       setError('There was an error fetching the public name, please contact the system administrator');
     }
-    setShowLoading(false);
   }
-  // ─────────────────────────────────────────────────────────────────────
+
   async function fetchUserAvatar(): Promise<void> {
     const userAvatarResultFromBack = await getUserAvatar();
     if (userAvatarResultFromBack) {
@@ -41,36 +31,35 @@ const Settings: React.FC = React.memo(() => {
     } else {
       setError('There was an error fetching the avatar, please contact the system administrator');
     }
-    setShowLoading(false);
   }
-  // ─────────────────────────────────────────────────────────────────────
 
   useEffect(() => {
-    fetchPublicName();
-    fetchUserAvatar();
+    setShowLoading(true); // Set showLoading to true before fetching the data
+
+    Promise.all([fetchPublicName(), fetchUserAvatar()])
+      .then(() => setShowLoading(false)) // Set showLoading to false after the data has been fetched
+      .catch(() => setShowLoading(false)); // Also set showLoading to false if there's an error
   }, []);
-/*
 
-     |         |             |                  ||
-,---.|--- ,---.|--- ,---.    |---.,---.,---.,---||    ,---.,---.,---.
-`---.|    ,---||    |---'    |   |,---||   ||   ||    |---'|    `---.
-`---'`---'`---^`---'`---'    `   '`---^`   '`---'`---'`---'`    `---'
 
-*/
   const checkUsernameAndUpdate = useCallback(async (): Promise<void> => {
-    if (newUsername) {
-      try {
-        const response = await updatePublicName(newUsername);
-        if (response.valid) {
-          setPublicName(newUsername);
-          alert('Username updated successfully!');
-          setIsUpdatingProfileName(false);
-        }
-      } catch (error) {
-        if (hasMessage(error)) {
-          setError(error.message);
-          alert(error.message);
-        }
+    // Check if newUsername is not empty
+    if (!newUsername) {
+      alert('Username cannot be empty');
+      return;
+    }
+
+    try {
+      const response = await updatePublicName(newUsername);
+      if (response.valid) {
+        setPublicName(newUsername);
+        alert('Username updated successfully!');
+        setIsUpdatingProfileName(false);
+      }
+    } catch (error) {
+      if (hasMessage(error)) {
+        setError(error.message);
+        alert(error.message);
       }
     }
   }, [newUsername]);
@@ -87,27 +76,17 @@ const Settings: React.FC = React.memo(() => {
         setIsUpdatingAvatar(false);
         alert('Avatar updated successfully!');
       } catch (error) {
-        if (hasMessage(error))
-          setError(error.message);
+        if (hasMessage(error)) setError(error.message);
       }
     }
   }, [newAvatar]);
 
-/*
-
-                  o
-        ,---.,---..,---.,---.,---.,---.
-        `---.|   |||   ||   ||---'|
-        `---'|---'``   '`   '`---'`
-             |
-*/
   useEffect(() => {
-
     let timer: NodeJS.Timeout;
     timer = setTimeout(() => {
       setShowLoading(false);
+    }, 1000); // Change the delay to 3000ms
 
-    }, 1000); // Change the delay to 2000ms
     return () => {
       clearTimeout(timer);
       setShowLoading(true);
@@ -119,74 +98,43 @@ const Settings: React.FC = React.memo(() => {
       <div className='container'>
         <LoadingSpinner />
       </div>
-    ); // Use a loading spinner instead of text
+    );
   }
 
-/*
-
-                  |
-        ,---.,---.|--- .   .,---.,---.
-        |    |---'|    |   ||    |   |
-        `    `---'`---'`---'`    `   '
-
-*/
   return (
     <div className='container'>
-
-      <CSSTransition
-        in={!isUpdatingProfileName} // Change this line
-        timeout={500}
-        classNames="fade"
-        appear
-      >
+      <CSSTransition in={!isUpdatingProfileName} timeout={500} classNames="fade" appear>
         <h3 className=''>{username}</h3>
       </CSSTransition>
-    {/* // ───────────────────────────────────────────────────────────────────── */}
+
       {isUpdatingProfileName && (
-        <CSSTransition in={true}
-          timeout={500}
-          classNames="fade"
-          unmountOnExit
-          onExited={() => setIsUpdatingProfileName(false)}
-        >
+        <CSSTransition in={true} timeout={500} classNames="fade" unmountOnExit onExited={() => setIsUpdatingProfileName(false)}>
           <>
             <input type="text" value={newUsername ?? ''} onChange={e => setNewPublicName(e.target.value)} />
             <button onClick={checkUsernameAndUpdate}>Confirm</button>
           </>
         </CSSTransition>
       )}
-      <button onClick={() => setIsUpdatingProfileName(!isUpdatingProfileName)}>
-        Update Public Name
-      </button>
-    {/* // ───────────────────────────────────────────────────────────────────── */}
-      <CSSTransition
-        in={!isUpdatingAvatar} // Change this line
-        timeout={500}
-        classNames="fade"
-        appear
-      >
+
+      <button onClick={() => setIsUpdatingProfileName(!isUpdatingProfileName)}> Update Public Name </button>
+
+      <CSSTransition in={!isUpdatingAvatar} timeout={500} classNames="fade" appear>
         <img className='img' src={avatarUrl ?? ''} alt={`${username ?? 'User'} avatar`} />
       </CSSTransition>
-    {/* // ───────────────────────────────────────────────────────────────────── */}
+
       {isUpdatingAvatar && (
-        <CSSTransition
-          in={isUpdatingAvatar}
-          timeout={500}
-          classNames="fade"
-          unmountOnExit
-          onExited={() => setIsUpdatingAvatar(false)} // Reset the state after the exit transition
-        >
+        <CSSTransition in={isUpdatingAvatar} timeout={500} classNames="fade" unmountOnExit onExited={() => setIsUpdatingAvatar(false)}>
           <>
             <input type="file" onChange={e => e.target.files && checkAndShowInputAvatar(e.target.files[0])} />
             <button onClick={sendNewAvatarToBack}>Confirm</button>
           </>
         </CSSTransition>
       )}
-      <button onClick={() => setIsUpdatingAvatar(!isUpdatingAvatar)}>
-        Update Avatar
-      </button>
-    {/* // ───────────────────────────────────────────────────────────────────── */}
+
+      <button onClick={() => setIsUpdatingAvatar(!isUpdatingAvatar)}> Update Avatar </button>
+
       <SignOutLink />
+
       {promptError && <p>{promptError}</p>}
     </div>
   );
