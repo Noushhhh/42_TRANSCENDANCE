75e1d0491a87c76d7e34242b04b1fa909ffc2973
polo <you@example.com>
2023-11-07 10:15:18 +0100

    feat: display sender's message username in chat to know who is talking in channel with more than 2 person

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -11,7 +11,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ChatModule } from './chat/chat.module';
 import { GameModule } from './game/game.module';
-import { ChatGateway } from './chat/chat.gateway';
 import { SocketService } from './chat/socket.service';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
@@ -26,6 +25,6 @@ import { JwtService } from '@nestjs/jwt';
     GameModule
   ],
   controllers: [AppController, ChatController],
-  providers: [AppService, ChatService, PrismaService, ChatGateway, SocketService, AuthService, JwtService],
+  providers: [AppService, ChatService, PrismaService, SocketService, AuthService, JwtService],
 })
 export class AppModule {}
