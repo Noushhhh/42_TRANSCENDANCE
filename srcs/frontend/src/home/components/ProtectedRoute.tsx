8392f4bf08cd40a0d9954186470e5fa82a6aad41
Alexvc23 <alexandervalencia1994@gmail.com>
2023-11-26 10:52:30 +0100

    Refactor ProtectedRoute to Enhance Lifecycle Logic
    This commit refactors the ProtectedRoute component to improve its lifecycle management. Changes include:
    
    1. Initializing the `isClientRegistered` state to `null` instead of `false` to defer navigation until after the completion of the asynchronous check for client registration status.
    2. Separating the useEffect hooks for token expiration check and client registration status check for clearer logic and better control over asynchronous operations.
    3. Adjusting conditional rendering to ensure navigation to either '/signin' or '/userprofilesetup' only occurs after the relevant state (tokenExpired, isClientRegistered) is definitively set.
    4. Implementing explicit checks (`=== true` and `=== false`) in conditional statements for better readability and to avoid unintended truthy/falsy evaluations.
    
    These updates ensure more predictable and reliable behavior of the component, particularly in how it handles asynchronous operations and conditional navigation.

--- srcs/frontend/src/home/components/ProtectedRoute.tsx
+++ srcs/frontend/src/home/components/ProtectedRoute.tsx
@@ -14,6 +14,18 @@ const MIN_LOADING_TIME = 2000;
 
 // ProtectedRoute functional component
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+
+    /*
+        d888
+       d8888
+        888
+        888
+        888
+        888
+        888   d8b
+      8888888 Y8P
+    
+    */
     // State to track if the token has expired
     const [tokenExpired, setTokenExpired] = useState<boolean | null | undefined>(null);
     // State to control the visibility of the loading screen
@@ -25,6 +37,21 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     const checkToken = useTokenExpired();
     const checkIsClientRegistered = useIsClientRegistered();
 
+    // ─────────────────────────────────────────────────────────────────────
+
+    /*
+     .d8888b.
+    d88P  Y88b
+           888
+         .d88P
+     .od888P"
+    d88P"
+    888"
+    888888888
+    
+    
+    
+    */
     // useEffect hook for checking token expiration
     useEffect(() => {
         const updateTokenStatus = async () => {
@@ -35,6 +62,20 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
 
         updateTokenStatus();
     }, [checkToken]); // Dependency array with checkToken to rerun if checkToken changes
+    // ─────────────────────────────────────────────────────────────────────────────
+    /*
+     .d8888b.
+    d88P  Y88b
+         .d88P
+        8888"
+         "Y8b.
+    888    888
+    Y88b  d88P
+     "Y8888P"
+    
+    
+    
+    */
 
     // useEffect hook for checking client registration
     useEffect(() => {
@@ -47,28 +88,101 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
         updateFirstConnectionStatus();
     }, [isClientRegistered]); // Dependency array with isClientRegistered to rerun if it changes
 
+    // ─────────────────────────────────────────────────────────────────────
+
+    /*
+                             d8888
+                            d8P888
+                           d8P 888
+                          d8P  888
+                         d88   888
+                         8888888888
+                               888
+                               888
+    
+    
+    
+    */
     // useEffect hook to handle the loading screen timeout
     useEffect(() => {
         const loadingTimeout = setTimeout(() => setShowLoader(false), MIN_LOADING_TIME);
         return () => clearTimeout(loadingTimeout); // Cleanup function to clear the timeout
     }, []);
 
+    // ─────────────────────────────────────────────────────────────────────────────
+    /*
+    888888888
+    888
+    888
+    8888888b.
+         "Y88b
+           888
+    Y88b  d88P
+     "Y8888P"
+    
+    
+    
+    */
+
     // Conditional rendering based on the token expiration state
     if (tokenExpired === true) {
         console.log(`passing by condition tokenExpired ${tokenExpired}`);
         return <Navigate to='/signin' />; // Redirect to the sign-in page if the token has expired
     }
+    // ─────────────────────────────────────────────────────────────────────
+    /*
+     .d8888b.
+    d88P  Y88b
+    888
+    888d888b.
+    888P "Y88b
+    888    888
+    Y88b  d88P
+     "Y8888P"
+    
+    
+    
+    */
+
     // Conditional rendering based on the client registration state
     else if (isClientRegistered === false) {
         console.log(`passing by condition isClientRegistered ${isClientRegistered}`);
         return <Navigate to='/userprofilesetup' />; // Redirect to the user profile setup if the client is not registered
     }
+    // ─────────────────────────────────────────────────────────────────────────────
+    /*
+    8888888888
+          d88P
+         d88P
+        d88P
+     88888888
+      d88P
+     d88P
+    d88P
+    
+    
+    
+    */
 
     // Display the loader while checking token or registration status
     if (showLoader || tokenExpired === null || isClientRegistered === null) {
         return <SkeletonLoader />;
     }
+    // ─────────────────────────────────────────────────────────────────────
 
+    /*
+     .d8888b.
+    d88P  Y88b
+    Y88b. d88P
+     "Y88888"
+    .d8P""Y8b.
+    888    888
+    Y88b  d88P
+     "Y8888P"
+    
+    
+    
+    */
     // Render the children components if all conditions are met
     return <>{children}</>;
 }
