4e1bbc3c8fca4ea23405289a80f39064218978d4
Alexvc23 <alexandervalencia1994@gmail.com>
2023-12-04 11:41:46 +0100

    Refactor UserProfileSetup for Enhanced Error Handling and State Management
    - Implement better state management for profile image and user email.
    - Optimize image preview updating to handle memory leaks.
    - Improve error handling by displaying user-friendly messages.
    - Enhance form handling with useCallback for optimized performance.
    - Update component to fetch and set default profile image on error during avatar update.
    - Code cleanup for readability and maintainability.

--- srcs/frontend/src/home/components/UserProfileSetup.tsx
+++ srcs/frontend/src/home/components/UserProfileSetup.tsx
@@ -1,77 +1,74 @@
-import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
-import {useNavigate } from "react-router-dom";
-import "../styles/generalStyles.css";
-import { useSignOut } from "../tools/hooks/useSignOut";
-import { fetchImageAsFile, useUpdateAvatar, useUpdatePublicName } from "../tools/Api";
-import LoadingSpinner from "../tools/LoadingSpinner";
-import { getUserData, hasMessage } from "../tools/Api";
+import React, { useState, useCallback, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/generalStyles.css';
+import { fetchImageAsFile, useUpdateAvatar, useUpdatePublicName, getUserData, hasMessage } from '../tools/Api';
+import LoadingSpinner from '../tools/LoadingSpinner';
 
 const defaultImage = 'defaultProfileImage.jpg';
 
 const UserProfileSetup: React.FC = React.memo(() => {
-  const [profileName, setProfileName] = useState<string | null>(null);
+  const [profileName, setProfileName] = useState('');
   const [profileImage, setProfileImage] = useState<File | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const { updateAvatar, isLoading: isAvatarUpdating} = useUpdateAvatar();
-  const { updatePublicName, isLoading: isPublicNameLoading} = useUpdatePublicName();
-  const [email, setEmail] = useState<string | null>(null);
+  const [profileImageUrl, setProfileImageUrl] = useState(defaultImage);
+  const [errorMessage, setErrorMessage] = useState('');
+  const { updateAvatar, isLoading: isAvatarUpdating } = useUpdateAvatar();
+  const { updatePublicName, isLoading: isPublicNameLoading } = useUpdatePublicName();
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
-  const handleSignOut = useSignOut();
 
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-  const profileNameRef = React.useRef<HTMLInputElement>(null);
-
+  // Fetch user data on mount
   useEffect(() => {
-    const getUserInfo = async () => {
+    const fetchUserInfo = async () => {
       try {
-        const result = await getUserData();
-        setEmail(result.publicName ? result.publicName : result.email);
+        const userInfo = await getUserData();
+        setEmail(userInfo.publicName || userInfo.email);
+        // Initialize with default profile image
+        const defaultProfileImage = await fetchImageAsFile(defaultImage, "defaultImage");
+        setProfileImage(defaultProfileImage);
       } catch (error) {
-        console.error(hasMessage(error) ? error.message : "Something went wrong when \
-        calling when fetching user email in UserProfile compoenet");
-      }
-    }
-
-    getUserInfo();
-    setDefaultImage();
-
-  }, [getUserData]);
-
-
-  useEffect(() => {
-    setErrorMessage(null);
-    return () => {
-      if (profileImage) {
-        URL.revokeObjectURL(URL.createObjectURL(profileImage));
+        setErrorMessage(hasMessage(error) ? error.message : 'Error fetching user data');
       }
     };
+    fetchUserInfo();
+  }, []);
+
+  // Update image preview
+  useEffect(() => {
+    if (profileImage) {
+      const imageUrl = URL.createObjectURL(profileImage);
+      setProfileImageUrl(imageUrl);
+      // Revoke URL to avoid memory leaks
+      return () => URL.revokeObjectURL(imageUrl);
+    }
   }, [profileImage]);
 
-  const setDefaultImage = async () => {
-    const defaultProfileImage = await fetchImageAsFile(defaultImage, "defaultImage");
-    setProfileImage(defaultProfileImage);
-  };
-
-  const handleImageChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files;
-    if (file)
-      setProfileImage(file[0]);
-  }, [profileImage]);
+  const handleImageChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setProfileImage(file);
+    }
+  }, []);
 
   const handleSubmit = useCallback(async () => {
+    if (!profileName) {
+      setErrorMessage('Please enter a profile name.');
+      return;
+    }
     try {
-      // Use the custom hook function to update the public name and avatar
       await updatePublicName(profileName);
       await updateAvatar(profileImage);
+      navigate('/home');
     } catch (error) {
-      console.error("Failed to update userProfileSetup:", error);
-     setErrorMessage(hasMessage(error)? error.message: "Failed to update ProfileName");
+      setErrorMessage(hasMessage(error) ? error.message : 'Failed to update profile');
+      try {
+        const defaultProfileImage = await fetchImageAsFile(defaultImage, "defaultImage");
+        setProfileImage(defaultProfileImage);
+      } catch (fetchError) {
+        console.error('Failed to fetch the default profile image:', hasMessage(fetchError) ? fetchError.message : '');
+      }
     }
-    navigate("/home");
-  }, [updateAvatar, updatePublicName, navigate]);
+  }, [profileName, profileImage, updateAvatar, updatePublicName, navigate]);
 
-
-  // Display loading spinner or error message based on the hook's states
   if (isAvatarUpdating || isPublicNameLoading) {
     return <LoadingSpinner />;
   }
@@ -79,15 +76,23 @@ const UserProfileSetup: React.FC = React.memo(() => {
   return (
     <div className="container">
       <h1>Pong game</h1>
-
-
-      {errorMessage && <div style={{ color: 'white' }}>{errorMessage}</div>}
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
       <h2>Welcome {email}</h2>
-
-
-      <input type="text" placeholder="Choose a username" ref={profileNameRef} onChange={e => {setErrorMessage(null); setProfileName(e.target.value); }} />
-      <input ref={fileInputRef} type="file" onChange={handleImageChange} />
-      <img className="img" src={profileImage ? URL.createObjectURL(profileImage) : defaultImage} alt="Profile preview" />
+      <input
+        type="text"
+        placeholder="Choose a username"
+        value={profileName}
+        onChange={e => setProfileName(e.target.value)}
+      />
+      <input
+        type="file"
+        onChange={handleImageChange}
+      />
+      <img
+        className="img"
+        src={profileImageUrl}
+        alt="Profile preview"
+      />
       <button onClick={handleSubmit}>Continue</button>
     </div>
   );
