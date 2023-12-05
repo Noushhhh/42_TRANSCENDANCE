dd997f9544869bd55d1803603de99fc7ac34d155
Alexvc23 <alexandervalencia1994@gmail.com>
2023-12-05 12:25:23 +0100

    feat: Enhance UserProfileSetup component
    - Added comments for better code documentation.
    - Improved code formatting and structure.
    - Separated functions for clarity and readability.

--- srcs/frontend/src/home/components/UserProfileSetup.tsx
+++ srcs/frontend/src/home/components/UserProfileSetup.tsx
@@ -16,28 +16,30 @@ import 'react-toastify/dist/ReactToastify.css';
 import useTokenExpired from '../tools/hooks/useTokenExpired';
 
 const defaultImage = 'defaultProfileImage.jpg';
-// Minimum time to display the loader
 const MIN_LOADING_TIME = 2000;
 
-// ─────────────────────────────────────────────────────────────────────────────
-
 const UserProfileSetup: React.FC = React.memo(() => {
-  // State for profile name, image, image URL, and loading screen visibility
+  // State for profile information
   const [profileName, setProfileName] = useState('');
   const [profileImage, setProfileImage] = useState<File | null>(null);
   const [profileImageUrl, setProfileImageUrl] = useState(defaultImage);
+  const [avatarFromBack, setAvatarFromBack] = useState<File | null>(null);
+
+  // API hooks
   const { updateAvatar, isLoading: isAvatarUpdating } = useUpdateAvatar();
   const { updatePublicName, isLoading: isPublicNameLoading } = useUpdatePublicName();
-  const [showLoader, setShowLoader] = useState(true); // Controls loading spinner visibility
   const isClientRegistered = useIsClientRegistered();
   const tokenExpired = useTokenExpired();
+
+  // Other state variables
+  const [showLoader, setShowLoader] = useState(true);
   const [email, setEmail] = useState('');
   const navigate = useNavigate();
 
   // Loading spinner timeout
   let loadingTimeout: NodeJS.Timeout;
 
-  // Set loading Spinner to false after MIN_LOADING_TIME passed
+  // Set loading spinner to false after MIN_LOADING_TIME passed
   const setLoaderSpinner = () => {
     loadingTimeout = setTimeout(() => {
       setShowLoader(false);
@@ -53,10 +55,13 @@ const UserProfileSetup: React.FC = React.memo(() => {
       setEmail(userInfo.publicName ?? userInfo.username);
       if (userInfo.avatar) {
         const avatar = await getUserAvatar();
-        setProfileImage(await fetchImageAsFile(avatar ?? " ", "avatar"));
+        const result = await fetchImageAsFile(avatar ?? " ", "avatar");
+        setAvatarFromBack(result);
+        setProfileImage(result);
       }
       else {
         // Initialize with default profile image
+        console.log(`setting default avatar`);
         const defaultProfileImage = await fetchImageAsFile(defaultImage, "defaultImage");
         setProfileImage(defaultProfileImage);
       }
@@ -81,51 +86,10 @@ const UserProfileSetup: React.FC = React.memo(() => {
     }
   }
 
-  // useEffect hook to handle the loading screen timeout
-  useEffect(() => {
-    setLoaderSpinner();
-    checkUserAuth();
-    fetchUserInfo();
-    return () => clearTimeout(loadingTimeout); // Cleanup function to clear the timeout
-  }, []);
-
-  // Update image preview when profileImage changes
-  useEffect(() => {
-    if (profileImage) {
-      const imageUrl = URL.createObjectURL(profileImage);
-      setProfileImageUrl(imageUrl);
-      // Revoke URL to avoid memory leaks
-      return () => URL.revokeObjectURL(imageUrl);
-    }
-  }, [profileImage]);
-
-  // Handle file input change to update profileImage
-  const handleImageChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      setProfileImage(file);
-    }
-  }, []);
-
-  // Handle updating the public name
-  const handleUpdatePublicName = useCallback(async () => {
-    if (!profileName) {
-      toast.error('Please enter a profile name.');
-      return;
-    }
-    try {
-      setShowLoader(true);
-      setLoaderSpinner();
-      await updatePublicName(profileName);
-      toast.success('Profile name updated successfully!');
-    } catch (error) {
-      toast.error(hasMessage(error) ? error.message : 'Failed to update profile name');
-    }
-  }, [profileName, updatePublicName, navigate]);
-
   // Handle updating the avatar
   const handleUpdateAvatar = useCallback(async () => {
     try {
+      console.log(`passing handleUpdateAvatar`);
       setShowLoader(true);
       setLoaderSpinner();
       await updateAvatar(profileImage);
@@ -145,13 +109,62 @@ const UserProfileSetup: React.FC = React.memo(() => {
   const checkAndNavigate = useCallback(async () => {
     try {
       const result = await isClientRegistered();
-      if (result) navigate('/home');
+      if (result) {
+        console.log(`passing by checkAndNavigate avatarFromBack: ${avatarFromBack}`);
+        if (!avatarFromBack) {
+          await handleUpdateAvatar();
+        }
+        navigate('/home');
+      }
       toast.error("You need to provide a Public Name to access the game");
     } catch (error) {
       console.error('passing by catch checkAndNavigate');
       toast.error("You need to provide a Public Name to access the game");
     }
-  }, [])
+  }, [profileImage,avatarFromBack, handleUpdateAvatar, isClientRegistered])
+
+  // useEffect hook to handle the loading screen timeout
+  useEffect(() => {
+    setLoaderSpinner();
+    checkUserAuth();
+    fetchUserInfo();
+    return () => clearTimeout(loadingTimeout); // Cleanup function to clear the timeout
+  }, []);
+
+  // Update image preview when profileImage changes
+  useEffect(() => {
+    if (profileImage) {
+      const imageUrl = URL.createObjectURL(profileImage);
+      setProfileImageUrl(imageUrl);
+      // Revoke URL to avoid memory leaks
+      return () => URL.revokeObjectURL(imageUrl);
+    }
+  }, [profileImage, avatarFromBack]);
+
+  // Handle file input change to update profileImage
+  const handleImageChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setProfileImage(file);
+    }
+  }, []);
+
+  // Handle updating the public name
+  const handleUpdatePublicName = useCallback(async () => {
+    if (!profileName) {
+      toast.error('Please enter a profile name.');
+      return;
+    }
+    try {
+      setShowLoader(true);
+      setLoaderSpinner();
+      await updatePublicName(profileName);
+      setEmail(profileName);
+      toast.success('Profile name updated successfully!');
+    } catch (error) {
+      toast.error(hasMessage(error) ? error.message : 'Failed to update profile name');
+    }
+  }, [profileName, updatePublicName, navigate]);
 
   // Render loading spinner or user profile setup form
   if (showLoader || isAvatarUpdating || isPublicNameLoading) {
