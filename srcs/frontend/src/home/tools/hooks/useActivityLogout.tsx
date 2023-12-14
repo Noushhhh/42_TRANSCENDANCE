68e4284a706b1a6d01e66ed84c233d01ea8c7d86
polo <you@example.com>
2023-12-14 13:28:33 +0100

    129 to main

--- srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
+++ srcs/frontend/src/home/tools/hooks/useActivityLogout.tsx
@@ -34,10 +34,7 @@ const useActivityLogout = (timeToLogout = 1000 * 60 * 20, refreshCheckInterval =
     };
 
     const updateLastActivity = () => {
-<<<<<<< HEAD
-=======
       //console.log("Updating last activity time");
->>>>>>> f42115fe4ac1e862a3fde3e8c735ec4739c8b2d8
       localStorage.setItem("lastActivity", Date.now().toString());
       resetTimer();
     };
