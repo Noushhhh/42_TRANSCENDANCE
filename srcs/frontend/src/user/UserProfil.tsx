ad2dec26f9a4b7ef60c706efd02a82be8952fce5
Théo <74186192+Theoted@users.noreply.github.com>
2023-12-28 15:02:53 +0100

    i've change all the fetch base url

--- srcs/frontend/src/user/UserProfil.tsx
+++ srcs/frontend/src/user/UserProfil.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect, useState } from "react";
 import { isUrlContainsHttps } from "../home/components/friendsComponents/FriendsList";
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 interface UserProfilProps {
   isDisplay: boolean;
   setIsDisplay: React.Dispatch<React.SetStateAction<boolean>>;
@@ -52,7 +54,7 @@ const UserProfil: FC<UserProfilProps> = ({
     const fetchUser = async () => {
       try {
         const response = await fetch(
-          "http://localhost:4000/api/users/getUserProfil",
+          `${API_BASE_URL}/api/users/getUserProfil`,
           {
             method: "GET",
             credentials: "include",
@@ -86,13 +88,12 @@ const UserProfil: FC<UserProfilProps> = ({
           gamePlayed: userProfile.gamePlayed,
           gameWon: winPercentage_,
         });
-
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchUser().catch(e => console.error(e));
+    fetchUser().catch((e) => console.error(e));
   }, []);
 
   if (!isDisplay) return <></>;
