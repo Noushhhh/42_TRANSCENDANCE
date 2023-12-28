2d2e3829814d69459aaca1919aeca993d66c9a61
ALEX <alex@ALEXs-MacBook-Pro.local>
2023-12-28 10:47:54 +0100

    Comment Out LoggerMiddleware Console Logs
    Commented out console log statements in the LoggerMiddleware for cleaner logs.

--- srcs/backend/src/middleWares/logger.middleware.ts
+++ srcs/backend/src/middleWares/logger.middleware.ts
@@ -5,8 +5,8 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const clientIp = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log("passing by middleware alex");
-    console.log(`Client IP: ${clientIp}`);
+    // console.log("passing by middleware alex");
+    // console.log(`Client IP: ${clientIp}`);
     next();
   }
 }
