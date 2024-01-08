edfcf28c2dff3bb37b041108ddf895c17efcd23d
polo <you@example.com>
2024-01-08 14:36:10 +0100

    update: remove unexistant middleware in AppModule

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -12,7 +12,6 @@ import { GameModule } from './game/game.module';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { StatsModule } from './stats/stats.module';
-import { LoggerMiddleware } from './middleWares/logger.middleware';
 
 @Module({
   imports: [
@@ -29,8 +28,5 @@ import { LoggerMiddleware } from './middleWares/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('*'); // Apply for all routes
   }
 }
